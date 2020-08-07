65ce5b8be0ffd9224872c5145f274490a59025dc
tom <tomdamri@gmail.com>
2020-08-07 18:48:17 +0300

    fixed - routes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import FormBuilderPage from './Routes/FormBuilderPage/FormBuilderPage';
 import FormSubmissionsPage from './Routes/FormSubmissionsPage';
 import FormSubmitPage from './Routes/FormSubmitPage/FormSubmitPage';
 import { Route, Switch, NavLink } from 'react-router-dom';
+import FormsListPage from './Routes/FormsListPage';
 
 const App = () => {
 
   return (
     <div className="App">
       <nav className='App-nav'>
+        <NavLink exact activeClassName="active-link" to="/">View All Forms</NavLink>
         <NavLink exact activeClassName="active-link" to="/form-builder-page">Build a Form</NavLink>
-        <NavLink exact activeClassName="active-link" to="/form-submissions-page">View All Forms</NavLink>
       </nav>
       <div>
       <Switch>
+        <Route exact path="/" component={FormsListPage} />
         <Route exact path="/form-builder-page" component={FormBuilderPage} />
-        <Route exact path="/form-submissions-page" component={FormSubmissionsPage} />
         <Route exact path="/submit-form/:id" component={FormSubmitPage} />
+        <Route exact path="/view-form-submissions/:id" component={FormSubmissionsPage}/>
       </Switch>
       </div>
     </div>
