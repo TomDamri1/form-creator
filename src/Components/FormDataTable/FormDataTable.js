e99a7e18a8802d7d155260130f98974c58d4be39
tom <tomdamri@gmail.com>
2020-08-07 18:51:05 +0300

    refactored

--- src/Components/FormDataTable/FormDataTable.js
+++ src/Components/FormDataTable/FormDataTable.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Table from '@material-ui/core/Table';
 import TableContainer from '@material-ui/core/TableContainer';
-import StaticTableHead from './StaticTableHead';
+import StaticTableHead from '../StaticTableHead';
 import TableBodyWithDataProp from './TableBodyWithDataProp';
 
 
@@ -11,7 +11,7 @@ const FormDataTable = (props) => {
     return (
         <div>
             <TableContainer>
-                <Table size="small" aria-label="a dense table">
+                <Table size="big" aria-label="a dense table">
                     <StaticTableHead headerCellsArray={tableHeaderArray}/>
                     <TableBodyWithDataProp data={data} />
                 </Table>
--- src/Components/FormDataTable/TableBodyWithDataProp.js
+++ src/Components/FormDataTable/TableBodyWithDataProp.js
@@ -1,32 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import TableRow from '@material-ui/core/TableRow'
-import TableCell from '@material-ui/core/TableCell';
 import TableBody from '@material-ui/core/TableBody';
-import { NavLink } from 'react-router-dom';
-
-
-
-const createTableRows = (data) => {
-    return data.map(row =>
-        <TableRow key={row.id} style={generateRowStyle(row.id)} >
-            <TableCell>{row.id}</TableCell>
-            <TableCell>{row.name}</TableCell>
-            <TableCell>{row.numberOfSumbissions}</TableCell>
-            <TableCell>
-                <NavLink exact to={`/submit-form/${row.id}`}>View</NavLink>
-            </TableCell>
-            <TableCell>{row.numberOfSumbissions}</TableCell>
-        </TableRow>
-    )
-}
+import createTableRows from './CreateTableRows';
 
 const TableBodyWithDataProp = props => {
     return (
         <TableBody>
-            {
-                createTableRows(props.data)
-            }
+            {createTableRows(props.data)}
         </TableBody>
     )
 }
@@ -36,5 +16,3 @@ TableBodyWithDataProp.propTypes = {
 }
 
 export default TableBodyWithDataProp
-
-const generateRowStyle = (id) => id % 2 === 0 ? { backgroundColor: "#e3f2fd" } : {};
