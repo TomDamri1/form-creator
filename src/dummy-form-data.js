2bcb7080fed136354b2bcc55141d42af677b50ee
tom <tomdamri@gmail.com>
2020-08-07 18:48:58 +0300

    change data to new scheme

--- src/dummy-form-data.js
+++ src/dummy-form-data.js
@@ -1,45 +1,58 @@
+const exampleObject = {
+    "id" : "1",
+    "name": "Form1 with space",
+    "fields": [
+        { "id": "0", "fieldLabel": "A1", "inputName": "N-A1", "inputType": "tel" },
+        { "id": "1", "fieldLabel": "A2", "inputName": "N-A2", "inputType": "date" },
+        { "id": "2", "fieldLabel": "A3", "inputName": "N-A3", "inputType": "color" },
+        { "id": "3", "fieldLabel": "A4", "inputName": "N-A4", "inputType": "text" },
+        { "id": "4", "fieldLabel": "A5", "inputName": "N-A5", "inputType": "email" },
+        { "id": "5", "fieldLabel": "A6", "inputName": "N-A6", "inputType": "number" }
+    ],
+    "answers": [
+        [
+            "054-4444444",
+            "2020-08-13",
+            "#005500",
+            "abcdefghijklmnop",
+            "tomdam@gmail.com",
+            "12345",
+        ],
+        [
+            "054-4444444",
+            "2020-08-13",
+            "#012340",
+            "abcdefghijklmnop",
+            "tomdam@gmail.com",
+            "12345",
+        ],
+        [
+            "054-4444444",
+            "2020-08-13",
+            "#00FFF0",
+            "abcdefghijklmnop",
+            "tomdam@gmail.com",
+            "12345",
+        ],
+        [
+            "054-4444444",
+            "2020-08-13",
+            "#012340",
+            "abcdefghijklmnop",
+            "tomdam@gmail.com",
+            "12345",
+        ],
+        [
+            "054-4444444",
+            "2020-08-13",
+            "#00FFF0",
+            "abcdefghijklmnop",
+            "tomdam@gmail.com",
+            "12345",
+        ],
+    ],
+}
+
 export default [
-    {
-        "id" : "1",
-        "name": "Form1 with space",
-        "fields": [
-            { "id": "0", "fieldLabel": "A1", "inputName": "N-A1", "inputType": "tel" },
-            { "id": "1", "fieldLabel": "A2", "inputName": "N-A2", "inputType": "date" },
-            { "id": "2", "fieldLabel": "A3", "inputName": "N-A3", "inputType": "color" },
-            { "id": "3", "fieldLabel": "A4", "inputName": "N-A4", "inputType": "text" },
-            { "id": "4", "fieldLabel": "A5", "inputName": "N-A5", "inputType": "email" },
-            { "id": "5", "fieldLabel": "A6", "inputName": "N-A6", "inputType": "number" }
-        ],
-        "numberOfSumbissions" : "5"
-    },
-    {
-        "id" : "2",
-        "name": "Form2",
-        "fields": [
-            { "id": "0", "fieldLabel": "A1", "inputName": "A1", "inputType": "tel" },
-            { "id": "1", "fieldLabel": "A2", "inputName": "A2", "inputType": "text" },
-            { "id": "2", "fieldLabel": "A3", "inputName": "A3", "inputType": "text" }
-        ],
-        "numberOfSumbissions" : "5"
-    },
-    {
-        "id" : "3",
-        "name": "Form3",
-        "fields": [
-            { "id": "0", "fieldLabel": "A1", "inputName": "A1", "inputType": "tel" },
-            { "id": "1", "fieldLabel": "A2", "inputName": "A2", "inputType": "text" },
-            { "id": "2", "fieldLabel": "A3", "inputName": "A3", "inputType": "text" }
-        ],
-        "numberOfSumbissions" : "5"
-    },
-    {
-        "id" : "4",
-        "name": "Form4",
-        "fields": [
-            { "id": "0", "fieldLabel": "A1", "inputName": "A1", "inputType": "tel" },
-            { "id": "1", "fieldLabel": "A2", "inputName": "A2", "inputType": "text" },
-            { "id": "2", "fieldLabel": "A3", "inputName": "A3", "inputType": "text" }
-        ],
-        "numberOfSumbissions" : "5"
-    },
+    exampleObject, exampleObject, exampleObject
 ]
