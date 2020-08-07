7cd8b53195dd53521c6ce7688c649de127abbc42
tom <tomdamri@gmail.com>
2020-08-07 18:51:39 +0300

    added - form submissions page

--- src/Routes/FormSubmissionsPage.js
+++ src/Routes/FormSubmissionsPage.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import data from '../dummy-form-data';
-import FormDataTable from '../Components/FormDataTable/FormDataTable';
+import FormSubmissionsTable from '../Components/FormSubmissionsTable/FormSubmissionsTable';
 
-const FormSubmissionsPage = () => {
-    
+const getFormSubmissionsFromServer = (id) => {
+    let myForm = false;
+    data.map(form => (form.id === id) ? myForm = form : '')
+    return myForm;
+}
+
+
+const FormSubmissionsPage = (props) => {
+    const thisForm = getFormSubmissionsFromServer(props.match.params.id);
+    console.log(thisForm)
     return (
         <div>
             <h1>Form Submissions Page</h1>
-            <FormDataTable data={data}/>
+            <FormSubmissionsTable data={thisForm}/>
         </div>
     )
 }
